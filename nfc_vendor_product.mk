c9f3938dd91cc01da6b7fc6113c908e9316eab6d
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-07-20 01:44:03 +0000

    STNFC: Remove condition to use NFC_PERMISSIONS_DIR
    [1] - we will always use stnfc here, whether HIDL or AIDL
    
    Change-Id: I5ef00bfca4745c32f2f0471316202777f9729c30
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -6,7 +6,6 @@ ST_VENDOR_NFC += android.hardware.nfc@1.2-service.st
 endif
 ST_VENDOR_NFC += nfc_nci.st21nfc.default
 
-ifeq ($(strip $(TARGET_USES_ST_NFC) $(TARGET_USES_ST_AIDL_NFC)),true)
 ifneq ($(TARGET_NFC_SKU),)
 NFC_PERMISSIONS_DIR := $(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(TARGET_NFC_SKU)
 else
@@ -25,7 +24,6 @@ PRODUCT_COPY_FILES += \
 ifeq ($(strip $(TARGET_USES_ST_AIDL_NFC)),true)
 ifeq ($(TARGET_NFC_SKU),)
 ST_VENDOR_NFC += nfc-service-default.xml
-endif
 endif
 
 PRODUCT_PACKAGES += $(ST_VENDOR_NFC)
